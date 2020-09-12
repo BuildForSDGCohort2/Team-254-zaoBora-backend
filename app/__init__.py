798ccbd8d44d5473b56ba86555935cdee872cd54
none <none@localhost.localdomain>
2020-09-12 12:16:59 +0300

    configure mpesa transaction apis

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,12 @@ from flask_jwt_extended import (JWTManager)
 
 from instance.config import app_config
 from app.database import Initialize_DB
+from flask_mpesa import MpesaAPI
+
+
+
+
+
 
 def create_app(config_name):
     # func to initialize Flask app
@@ -12,22 +18,27 @@ def create_app(config_name):
     # init app
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
-    app.config.from_object(app_config[config_name])
+    #app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
     # init JWT
-    jwt = JWTManager(app)
+    jwt = JWTManager()
+    jwt.init_app(app)
 
     # v1 Blueprints
     from app.api.v1.views.user_view import v1 as user_v1
 
     app.register_blueprint(user_v1)
+    # instantiate mpesa
+    mpesaapi= MpesaAPI()
 
+    #init mpesa api on app
+    mpesaapi.init_app(app)
     # init db
-    db = Initialize_DB(app_config[config_name])
-    db.init_db(app_config[config_name])
-    db.create_tables()
-    db.connection.commit
+    db = Initialize_DB(app)
+    #db.init_db(app)
+    #db.create_tables()
+    #db.connection.commit
 
     # @jwt.token_in_blacklist_loader
     # def check_blacklisted(token):
@@ -53,5 +64,6 @@ def create_app(config_name):
         # handler for error 405
 
         return jsonify({'status': 405, 'message': 'Method not allowed'}), 405
+    #@v1.route('/transact/c2b')
 
     return app
