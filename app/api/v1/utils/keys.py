fc74f95c7f1507b517b666f07273c7b788e7eeee
Derrick  Moseti <50165747+moseti1@users.noreply.github.com>
2020-09-22 16:00:04 +0300

    Delete keys.py

--- app/api/v1/utils/keys.py
+++
@@ -1,5 +0,0 @@
-business_ShortCode = "174379" # Lipa na mpesa Shortcode
-phone_number = '254700867925'
-consumer_key = "cNmnbwEstQ6s7Ks8oOz9oVjrCAoF7cXt"
-consumer_secret = "zGAlKvpMMG5YvDQS"
-lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
