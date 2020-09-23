67e20c267f2f8d679136325f67de44d0f769b6eb
David Macharia <macharia3041@gmail.com>
2020-09-23 22:08:11 +0300

    fixed init file

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 from flask_jwt_extended import (JWTManager)
 from flask_redis import FlaskRedis
 
-from instance.config import app_config
 from app.database import Initialize_DB
 from flask_mpesa import MpesaAPI
 from config.development import DATABASE_URI
