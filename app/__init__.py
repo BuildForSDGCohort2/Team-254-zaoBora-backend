c154a5fc63e315d874f07949ee917cf789b8729c
Dave-mash <41325679+Dave-mash@users.noreply.github.com>
2020-10-19 09:53:31 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,17 +17,15 @@ def create_app(config_name):
 
     # init app
     app = Flask(__name__, instance_relative_config=True)
-    
-    # Cors settings
-    app.config['CORS_HEADERS'] = "Content-Type"
-    app.config['CORS_RESOURCES'] = {r"/api/*": {"origins": "*"}}
-    CORS(app)
 
     # Load the default configuration
     app.config.from_object('config.default')
 
     # Load the development configuration
     app.config.from_object('config.development')
+    
+    # Cors
+    CORS(app)
 
     # Load the configuration from the instance folder
 
